484fba756a1b09595b5c8fcb213daebc8945550b
zahidul Islam <your@email.com>
2026-06-14 01:16:24 +0600

    my first website commit

---
+++ java s/variable.js
@@ -0,0 +1,17 @@
+console.log("10 + 22 = " + (10+22))
+console.log("10 - 22 = " + (10-22))
+console.log("10 / 22 = " + (10/22))
+console.log("10 * 22 = " + (10*22))
+
+
+console.log("Hello World")
+console.log("10 + 5 = " + (10+5))
+console.log("Welcome to JavaScript!")
+
+let a = 50;
+let b = 20;
+
+console.log("a + b = " + (a + b));
+console.log("a - b = " + (a - b));
+console.log("a * b = " + (a * b));
+console.log("আমাদের হিসাব শেষ!");
